b4d57ffe2060cb6acd87d096ef5aca8e8e090e1d
Manoel Rodrigues Loureiro <manoel.rl@gmail.com>
2022-12-02 21:54:14 -0300

    Iniciando servidor

---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
+const server = http.createServer(function(req, res){
+
+    // Pegar a URL e dar um parse
+    const parsedUrl = url.parse(req.url, true);
+
+
+    //Pegar o path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    //Get the query string as an object
+    const queryStringObject = parsedUrl.query;
+
+    //Get the HTTP Method
+    const method = req.method.toLowerCase();
+
+    //Get the headers as an object
+    const headers = req.headers;
+
+    //Get the payload, if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
+    req.on('end', function(){
+        buffer += decoder.end();
+
+
+        //Mandar a resposta
+        res.end('Hello World\n');
+
+        //Log the request path
+        console.log('Request receive with this payload ',buffer);
+        
+    });
+});
+
+
+// Dando start no servidor, e escutando a porta 3000
+server.listen(3000, function(){
+    console.log("The server is listening on port 3000 now");
+});
+
